9c2923b9f38e3761b6aea985204fa91ec76f4abb
ABEL\abelm <abelmanuellugtujr1996@gmail.com>
2022-08-31 20:03:57 +0800

    ch3

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,36 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(8),
-          // color: Colors.red,
-          width: 100,
-          height: 100,
-          clipBehavior: Clip.antiAlias,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
+      body: Container(
+        height: 100,
+        color: Colors.teal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
               color: Colors.red,
-              // shape: BoxShape.circle,
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(colors: [
-                Colors.pink,
-                Colors.yellow,
-              ]),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 10,
-                )
-              ]),
-          child: Text(
-            "I am a Box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
             ),
-          ),
+            Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.yellow,
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.green,
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+            ),
+          ],
         ),
       ),
     );
